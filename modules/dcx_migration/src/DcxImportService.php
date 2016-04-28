28772e2319c871b70b0141a3a204cb6bbdf3caad
Georg Oechsler <go@wunderkraut.com>
2016-04-28 09:29:09 +0200

    DRPLDCX-53 Change loading migration instances to plugin manger

--- modules/dcx_migration/src/DcxImportService.php
+++ modules/dcx_migration/src/DcxImportService.php
@@ -2,10 +2,10 @@
 
 namespace Drupal\dcx_migration;
 
-use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\StringTranslation\TranslationInterface;
 use Drupal\dcx_migration\DcxMigrateExecutable;
+use Drupal\migrate\Plugin\MigrationPluginManagerInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class DcxImportService implements DcxImportServiceInterface {
@@ -15,20 +15,16 @@ class DcxImportService implements DcxImportServiceInterface {
 
   protected $event_dispatcher;
 
-  public function __construct(TranslationInterface $string_translation, EntityTypeManager $entity_type_manager, EventDispatcherInterface $event_dispatcher) {
+  public function __construct(TranslationInterface $string_translation, MigrationPluginManagerInterface $plugin_manager , EventDispatcherInterface $event_dispatcher) {
     $this->stringTranslation = $string_translation;
-    $this->entity_type_manager = $entity_type_manager;
+    $this->plugin_manager = $plugin_manager;
     $this->event_dispatcher = $event_dispatcher;
   }
 
   protected function getMigrationExecutable() {
-    if (! $this->migration_executable) {
-      $migration = $this->entity_type_manager
-        ->getStorage('migration')
-        ->load('dcx_migration');
-
-      $this->migration_executable = new DcxMigrateExecutable($migration, $this->event_dispatcher);
-    }
+    $migration = $this->plugin_manager->createInstance('dcx_migration');
+    dpm($migration);
+    $this->migration_executable = new DcxMigrateExecutable($migration, $this->event_dispatcher);
 
     return $this->migration_executable;
   }
