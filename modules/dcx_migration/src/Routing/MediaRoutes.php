fef0defefcf85730c4b282ac9a717388fcd50f1b
Christian Fritsch <chr.fritsch@gmx.net>
2016-05-19 11:00:28 +0200

    DRPLDCX-21: Change add image form depending on provider

---
+++ modules/dcx_migration/src/Routing/MediaRoutes.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\dcx_migration\Routing\ExampleRoutes.
+ */
+
+namespace Drupal\dcx_migration\Routing;
+
+use Drupal\media_entity\Entity\MediaBundle;
+use Symfony\Component\Routing\Route;
+
+/**
+ * Defines dynamic routes.
+ */
+class MediaRoutes {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function routes() {
+
+    /** @var MediaBundle[] $bundles */
+    $bundles = \Drupal::entityTypeManager()->getStorage('media_bundle')->loadMultiple();
+
+    $routes = array();
+
+    foreach ($bundles as $bundle) {
+
+      if ($bundle->get('type') == 'image') {
+        $routes['dcx_migration.form.' . $bundle->id()] = new Route(
+        // Path to attach this route to:
+          'media/add/' . $bundle->id(),
+          // Route defaults:
+          array(
+            '_form' => '\Drupal\dcx_migration\Form\DcxImportForm',
+            '_title' => 'Import Image from DC-X'
+          ),
+          // Route requirements:
+          array(
+            '_entity_create_access' => 'media:' . $bundle->id(),
+          ),
+          array(
+            '_admin_route' => TRUE,
+          )
+        );
+      }
+
+    }
+
+    return $routes;
+  }
+}
