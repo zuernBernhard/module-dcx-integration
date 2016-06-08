2be4580880da038c7174c07ed4e129e249fe8c40
Christian Fritsch <chr.fritsch@gmx.net>
2016-06-08 12:28:01 +0200

    Add a dcx link field widget

---
+++ modules/dcx_migration/src/Plugin/Field/FieldWidget/DcxLinkFieldWidget.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\dcx_migration\Plugin\Field\FieldWidget\DcxLinkFieldWidget.
+ */
+
+namespace Drupal\dcx_migration\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * Plugin implementation of the 'dcx_link_field_widget' widget.
+ *
+ * @FieldWidget(
+ *   id = "dcx_link_field_widget",
+ *   label = @Translation("DCX-Link"),
+ *   field_types = {
+ *     "string"
+ *   }
+ * )
+ */
+class DcxLinkFieldWidget extends WidgetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element = [];
+
+    $dcxUrl = \Drupal::config('dcx_integration.jsonclientsettings')->get('url');
+
+    $baseUrl = pathinfo($dcxUrl)['dirname'];
+
+    $document = str_replace('dcxapi:document', 'doc', $items[$delta]->value);
+
+    $element['value'] = $element + array(
+        '#title' => t('View in DCX'),
+        '#type' => 'link',
+        '#url' => Url::fromUri($baseUrl . "/documents#/" . $document),
+        '#attributes' => ['target' => '_blank'],
+      );
+
+    return $element;
+  }
+
+}
